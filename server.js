4690948b38c21239036942ae4350f78e31ef058d
Your Name <yourname@example.com>
2024-09-24 23:33:33 -0400

    Second Commit

--- server.js
+++ server.js
@@ -4,17 +4,14 @@ const cors = require('cors');
 const crypto = require('crypto');
 const corsOptions = require('./config/corsOptions');
 const path = require('path')
-const connectDB = require('./config/dbConn');
 const verifyToken = require('./middleware/verifyToken');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use(cors(corsOptions));
-
-connectDB();
-
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
@@ -26,6 +23,13 @@ app.use('/signup', require('./routes/signupRoutes'))
 
 app.use('/login', require('./routes/loginRoutes'))
 
-app.listen(3000, () => {
-    console.log("Server listening on port 3000");
+mongoose.connect(process.env.MONGO_ID).then(() => {
+    app.listen(3000, () => {
+        console.log("Server listening on port 3000");
+    });
+    console.log("mongodb");
 });
+
+
+
+
