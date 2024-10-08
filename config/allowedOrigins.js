cbd4f4eb98dce071f358e86f5f8941d365083927
Adan Wu <162506142+adanWu2005@users.noreply.github.com>
2024-10-08 01:33:13 -0400

    Add files via upload

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'https://shapeshift-h0yu.onrender.com',
+    'https://shapeshift-backend-2dhd.onrender.com',
+    'http://localhost:3000'
+];
+
+module.exports = allowedOrigins;
