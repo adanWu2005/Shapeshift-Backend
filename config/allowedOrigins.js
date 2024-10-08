c707d410e3118426dfcee7b9b11ff48b39d7f4ba
Adan Wu <162506142+adanWu2005@users.noreply.github.com>
2024-10-08 16:38:22 -0400

    Delete config directory

--- config/allowedOrigins.js
+++
@@ -1,7 +0,0 @@
-const allowedOrigins = [
-    'https://shapeshift-h0yu.onrender.com',
-    'https://shapeshift-backend-2dhd.onrender.com',
-    'http://localhost:3000'
-];
-
-module.exports = allowedOrigins;
